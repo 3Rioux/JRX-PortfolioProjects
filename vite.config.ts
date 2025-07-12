0339956fb43cf303f117a008ff98abe087287458
3Rioux <jtherioux@gmail.com>
2025-07-12 12:42:09 -0300

    proper repo name lol

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JRX-PortfolioProjects.github.io/',
+  base: '/JRX-PortfolioProjects/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
