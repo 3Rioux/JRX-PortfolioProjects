1e6e1ff885f47fbcb2011b4cb5ad59035f9a4a54
3Rioux <jtherioux@gmail.com>
2025-07-12 12:21:19 -0300

    Error fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Children, useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Fuse from 'fuse.js';
 
 import { Input } from '@/components/ui/input';
